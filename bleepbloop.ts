51d94beec5547bd1a710efb9a7400c67ed4c094d
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-09-06 22:24:16 +0200

    Attempt at backpropagation [not working]

--- bleepbloop.ts
+++ bleepbloop.ts
@@ -7,37 +7,61 @@ bleepbloop.ts -- main file that binds the library together
 
 // Helper functions
 function randomWeight(): number {
-    return Math.random() * .2 - .1;
+    return Math.random() * .2;
 }
 
 function sigmoid(input: number): number {
     return 1 / (1 + Math.pow(Math.E, input));
 }
 
+function error(target: number[], output: number[]) {
+    return .5 * target.reduce((acc, curr, i) => {
+        if (i == 0) {
+            return Math.pow(acc - output[i], 2);
+        }
 
-class Unit {
+        return acc + Math.pow(curr - output[i], 2)
+    });
+}
+
+class Neuron {
     weights: number[];
+    value: number;
+    inputs: number[];
 
-    calculate(inputs: number[], bias: number) {
-        return sigmoid(inputs.reduce((acc, curr, i) => (
-            acc + curr * this.weights[i]
-        )) + bias);
+    activate(inputs: number[], bias: number) {
+        // For the backpropagation
+        this.inputs = inputs;
+
+        this.value = inputs.reduce((acc, curr, i) => {
+            if (i == 0) {
+                return acc * this.weights[i];
+            }
+
+            return acc + curr * this.weights[i]
+        }) + bias;
+
+        return this.value;
+    }
+
+    transfer(inputs: number[], bias: number) {
+        return sigmoid(this.activate(inputs, bias));
     }
 }
 
 class Layer {
-    units: Unit[];
+    neurons: Neuron[];
     bias: number;
 
     constructor(unitCount: number) {
-        this.units = Array.apply(null, new Array(unitCount)).map(() => (new Unit));
+        this.neurons = Array.apply(null, new Array(unitCount)).map(() => (new Neuron));
     }
 }
 
 class Network {
     layers: Layer[];
 
-    constructor(public layerLenghts: number[]) {
+    constructor(public layerLenghts: number[], public learningRate: number) {
         this.layers = layerLenghts.map((layerLength, i) => {
             return new Layer(layerLenghts[i]);
         });
@@ -54,42 +78,133 @@ class Network {
 
             layer.bias = randomWeight();
 
-            // doesn’t work
-            for (let unit of layer.units) {
-                unit.weights = Array.apply(null, new Array(this.layers[layerIndex - 1].units.length).map(() => (randomWeight())));
+            for (let unit of layer.neurons) {
+                unit.weights = Array.apply(null, new Array(this.layers[layerIndex - 1].neurons.length)).map(() => (
+                    randomWeight()
+                ));
             }
-            // end, broken stuff
         }
     }
 
-    run(inputs: number[]) {
-        if (inputs.length != this.layers[0].units.length) {
+    predict(inputs: number[]): number[] {
+        if (inputs.length != this.layers[0].neurons.length) {
             console.error('Input length must be the same as number of input units!');
-            return;
+            return [];
         }
 
-        let prevLayerOutputs: number[];
+        let prevLayerOutputs: number[] = inputs;
+
+        //console.log(prevLayerOutputs);
 
         for (let layerIndexStr in this.layers) {
             let layerIndex: number = Number(layerIndexStr);
 
-            if (layerIndex == 0) { prevLayerOutputs = inputs; }
+            if (layerIndex == 0) {
+                continue;
+            }
 
-            prevLayerOutputs = this.layers[layerIndex].units.map((unit: Unit) => (
-                unit.calculate(prevLayerOutputs, this.layers[layerIndex].bias)
+            prevLayerOutputs = this.layers[layerIndex].neurons.map((neuron) => (
+                neuron.transfer(prevLayerOutputs, this.layers[layerIndex].bias)
+            ));
+        }
+
+        return prevLayerOutputs;
+    }
+
+    train(trainingExamples: [number[]], trainingTargets: [number[]]) {
+        // Using backpropagation
+        if (trainingExamples[0].length != this.layers[0].neurons.length) {
+            console.error('Input length must be the same as number of input units!');
+            return;
+        }
+
+        if (trainingTargets[0].length != this.layers[this.layers.length - 1].neurons.length) {
+            console.error('Target length must be the same as number of utput units!');
+            return;
+        }
+
+        if (trainingExamples.length != trainingTargets.length) {
+            console.error('Examples must be of same length as targets!');
+            return;
+        }
+
+        for (let trainingExampleIndexStr in trainingExamples) {
+            let trainingExampleIndex = Number(trainingExampleIndexStr);
+
+            let example = trainingExamples[trainingExampleIndex];
+            let target = trainingTargets[trainingExampleIndex];
+
+            let pred = this.predict(example);
+
+            let outputErrorTerm = pred.map((pred, i) => (
+                pred * (1 - pred) * (target[i] - pred)
             ));
 
-            console.log(prevLayerOutputs);
+            let errorTermsForLayers: [number[]] = [outputErrorTerm];
+
+            for (let layerIndex = this.layers.length - 2; layerIndex > 0; layerIndex--) {
+                errorTermsForLayers.unshift(this.layers[layerIndex].neurons.map((neuron, i) => {
+                    let downstreamNeurons: Neuron[];
+
+                    if (layerIndex == this.layers.length - 2) {
+                        downstreamNeurons = this.layers[this.layers.length - 1].neurons;
+                    } else {
+                        downstreamNeurons = this.layers[layerIndex + 1].neurons;
+                    }
+
+                    let sumOfDownstreamWeightsAndErrorTerms: number = downstreamNeurons.map((neuron, j) => (
+                        neuron.weights[j]
+                    )).reduce((acc, curr, j) => {
+                        if (j == 0) {
+                            return acc * errorTermsForLayers[errorTermsForLayers.length - 1][j];
+                        }
+
+                        return acc + curr * errorTermsForLayers[errorTermsForLayers.length - 1][j];
+                    });
+
+                    return neuron.value * (1 - neuron.value) * sumOfDownstreamWeightsAndErrorTerms;
+                }));
+            }
+
+            // Update weights
+            for (let layerIndexStr in this.layers.slice(1)) {
+                let layerIndex = Number(layerIndexStr);
+
+                this.layers[layerIndex + 1].neurons.map((neuron, i) => {
+                    neuron.weights = neuron.weights.map((neuronWeight, j) => (
+                        neuronWeight + (this.learningRate * errorTermsForLayers[layerIndex][i] * neuron.inputs[j])
+                    ));
+                });
+            }
         }
     }
 }
 
-let network: Network = new Network([1, 2, 3]);
+let network: Network = new Network([2, 1], .05);
 
-for (let layer of network.layers.slice(1)) {
-    for (let unit of layer.units) {
-        console.log(unit.weights);
-    }
-}
+// Generate training data
 
-network.run([10]);
+let input1 = Array.apply(null, new Array(25000)).map(() => ([0, 0]));
+let input2 = Array.apply(null, new Array(25000)).map(() => ([0, 1]));
+let input3 = Array.apply(null, new Array(25000)).map(() => ([1, 0]));
+let input4 = Array.apply(null, new Array(25000)).map(() => ([1, 1]));
+
+let input = input1.concat(input2).concat(input3).concat(input4);
+
+let output1 = Array.apply(null, new Array(25000)).map(() => ([0]));
+let output2 = Array.apply(null, new Array(25000)).map(() => ([1]));
+let output3 = Array.apply(null, new Array(25000)).map(() => ([1]));
+let output4 = Array.apply(null, new Array(25000)).map(() => ([1]));
+
+let output = output1.concat(output2).concat(output3).concat(output4);
+
+// Finish generating training data
+
+network.train(input, output);
+
+console.log(network.layers[network.layers.length - 1].neurons[0].weights)
+
+console.log(network.predict([0, 0]));
+console.log(network.predict([1, 0]));
+console.log(network.predict([0, 1]));
+console.log(network.predict([1, 1]));
